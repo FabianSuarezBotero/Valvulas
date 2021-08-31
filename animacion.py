e6e3a4ba7f21cfd32f1853ff8772c07947f402be
FabianSuarezBotero <faesuarezb@correo.udistrital.edu.co>
2021-08-30 21:59:59 -0500

    Valvulas abiertas y cerradas

--- animacion.py
+++ animacion.py
@@ -19,7 +19,22 @@ canvas.pack()
 img = tk.PhotoImage(file="planta.gif")
 canvas.create_image(0, 0, anchor=tk.NW, image=img)
 
-canvas.create_rectangle(68, 85, 180, 150, width=5, fill='red')
-canvas.create_rectangle(290, 85, 400, 152, width=5, fill='cyan')
+canvas.create_rectangle(68, 85, 180, 150, width=5,
+                        fill='pink')  # Nivel tanque 1 lleno
+canvas.create_rectangle(290, 85, 400, 152, width=5,
+                        fill='cyan')  # Nivel Tanque 2 lleno
+
+# canvas.create_rectangle(68, 85, 180, 150, width=5,fill='white')  # Nivel tanque 1 vacio
+# canvas.create_rectangle(290, 85, 400, 152, width=5,fill='white')  # Nivel tanque 2 vacio
+
+canvas.create_line(175, 179, 147, 156, width=5,
+                   fill='red')  # Valvula 1 cerrada
+canvas.create_line(290, 179, 318, 156, width=5,
+                   fill='red')  # Valvula 2 cerrada
+
+canvas.create_line(175, 179, 147, 156, width=5,
+                   fill='green')  # Valvula 1 abierta
+canvas.create_line(290, 179, 318, 156, width=5,
+                   fill='green')  # Valvula 2 abierta
 
 tk.mainloop()
