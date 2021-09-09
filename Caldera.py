fbd449519faf34b1cf2d913bcdbdfc92c541d509
FabianSuarezBotero <faesuarezb@correo.udistrital.edu.co>
2021-09-09 01:14:48 -0500

    Terminado caldera

--- Caldera.py
+++ Caldera.py
@@ -4,15 +4,17 @@ from random import *
 
 
 class Caldera():
-    estado = 0
-    temperatura = 0
 
     def __init__(self, pantalla):
-        frame = Frame(pantalla)
-        frame.config(width=800, height=200, bg="blue", highlightthickness=0)
-        frame.pack(padx=10, pady=10)
+        self.estado = 0
+        self.grados = 25
 
-        self.caldera = Canvas(frame, width=800, height=200, bg='grey')
+        self.frame = Frame(pantalla)
+        self.frame.config(width=800, height=200,
+                          bg="blue", highlightthickness=0)
+        self.frame.pack(padx=10, pady=10)
+
+        self.caldera = Canvas(self.frame, width=800, height=200, bg='grey')
         self.caldera.place(x=100, y=100)
 
         self.caldera.create_rectangle(140, 30, 320, 165, fill="#CAC6BE")
@@ -24,12 +26,12 @@ class Caldera():
         self.caldera.create_polygon(100, 85, 70, 130, 130, 130, fill='white')
         self.caldera.create_polygon(100, 30, 70, 75, 130, 75, fill='white')
 
-        nombre1 = Label(raiz, text="Nivel 2", bg="black",
+        nombre1 = Label(self.frame, text="Nivel 2", bg="black",
                         fg="white", font="Helvetica 12 bold")
         nombre1.pack()
         nombre1.place(x=335, y=50)
 
-        nombre2 = Label(raiz, text="Nivel 1", bg="black",
+        nombre2 = Label(self.frame, text="Nivel 1", bg="black",
                         fg="white", font="Helvetica 12 bold")
         nombre2.pack()
         nombre2.place(x=335, y=110)
@@ -39,11 +41,11 @@ class Caldera():
         num = 149
         for i in range(110, num):
             self.caldera.create_rectangle(140, 30, 320, 165, fill="#CAC6BE")
-            self.caldera.create_oval(140, 150, 320, 170, fill="blue")
-            self.caldera.create_rectangle(140, num, 320, 165, fill="blue")
-            self.caldera.create_oval(140, 155, 320, 175, fill="blue")
+            self.caldera.create_oval(140, 150, 320, 170, fill="pink")
+            self.caldera.create_rectangle(140, num, 320, 165, fill="pink")
+            self.caldera.create_oval(140, 155, 320, 175, fill="pink")
             # tapa superior tanque liquido
-            self.caldera.create_oval(140, num-15, 320, num+15, fill="blue")
+            self.caldera.create_oval(140, num-15, 320, num+15, fill="pink")
 
             # tapa superior tanque vacio
             self.caldera.create_oval(140, 15, 320, 45, fill="#CAC6BE")
@@ -53,17 +55,23 @@ class Caldera():
             num = num - 1
             N = int((i-64)*250)
 
-            estado = 1
+            self.estado = 1
+
+    def mostrar_estado(self):
+        return self.estado
+
+    def mostrar_temp(self):
+        return self.temperatura
 
     def llenado2(self):
         num = 119
         for i in range(55, num):
             self.caldera.create_rectangle(140, 30, 320, 165, fill="#CAC6BE")
-            self.caldera.create_oval(140, 150, 320, 170, fill="blue")
-            self.caldera.create_rectangle(140, num, 320, 165, fill="blue")
-            self.caldera.create_oval(140, 155, 320, 175, fill="blue")
+            self.caldera.create_oval(140, 150, 320, 170, fill="pink")
+            self.caldera.create_rectangle(140, num, 320, 165, fill="pink")
+            self.caldera.create_oval(140, 155, 320, 175, fill="pink")
             # tapa superior tanque liquido
-            self.caldera.create_oval(140, num-15, 320, num+15, fill="blue")
+            self.caldera.create_oval(140, num-15, 320, num+15, fill="pink")
 
             # tapa superior tanque vacio
             self.caldera.create_oval(140, 15, 320, 45, fill="#CAC6BE")
@@ -73,57 +81,136 @@ class Caldera():
             num = num - 1
             N = int((i-64)*250)
 
-            estado = 2
+            self.estado = 2
 
-    def vaciado(self):
-        for a in range(55, 165):
-            self.caldera.create_rectangle(140, 30, 320, 165, fill="#CAC6BE")
-            self.caldera.create_oval(140, 150, 320, 170, fill="blue")
-            self.caldera.create_rectangle(140, a, 320, 165, fill="blue")
-            self.caldera.create_oval(140, 155, 320, 175, fill="blue")
-            # tapa superior tanque liquido
-            self.caldera.create_oval(140, a-15, 320, a+15, fill="blue")
+    def vaciado2(self):
+        while self.estado == 2:
 
-            # tapa superior tanque vacio
-            self.caldera.create_oval(140, 15, 320, 45, fill="#CAC6BE")
+            for a in range(55, 165):
+                self.caldera.create_rectangle(
+                    140, 30, 320, 165, fill="#CAC6BE")
+                self.caldera.create_oval(140, 150, 320, 170, fill="pink")
+                self.caldera.create_rectangle(140, a, 320, 165, fill="pink")
+                self.caldera.create_oval(140, 155, 320, 175, fill="pink")
+                # tapa superior tanque liquido
+                self.caldera.create_oval(140, a-15, 320, a+15, fill="pink")
 
-            self.caldera.update()
+                # tapa superior tanque vacio
+                self.caldera.create_oval(140, 15, 320, 45, fill="#CAC6BE")
 
-            time.sleep(0.015)
-            l = int(25000 - (a-64)*250)
+                self.caldera.update()
 
-            estado = 0
+                time.sleep(0.01)
+                l = int(25000 - (a-64)*250)
+
+                self.estado = 0
+
+    def vaciado1(self):
+        while self.estado == 1:
+
+            for a in range(110, 165):
+                self.caldera.create_rectangle(
+                    140, 30, 320, 165, fill="#CAC6BE")
+                self.caldera.create_oval(140, 150, 320, 170, fill="pink")
+                self.caldera.create_rectangle(140, a, 320, 165, fill="pink")
+                self.caldera.create_oval(140, 155, 320, 175, fill="pink")
+                # tapa superior tanque liquido
+                self.caldera.create_oval(140, a-15, 320, a+15, fill="pink")
+
+                # tapa superior tanque vacio
+                self.caldera.create_oval(140, 15, 320, 45, fill="#CAC6BE")
+
+                self.caldera.update()
+
+                time.sleep(0.01)
+                l = int(25000 - (a-64)*250)
+
+                self.estado = 0
 
     def subir_temperatura(self):
-        if temperatura == 0:
+        if self.grados < 100 and self.grados >= 75:
+            self.caldera.create_polygon(100, 30, 70, 75, 130, 75, fill='red')
+            self.caldera.create_polygon(
+                100, 140, 70, 185, 130, 185, fill='yellow')
+            self.caldera.create_polygon(
+                100, 85, 70, 130, 130, 130, fill='orange')
+            self.grados = 100
+
+        elif self.grados < 75 and self.grados >= 50:
+            self.caldera.create_polygon(
+                100, 85, 70, 130, 130, 130, fill='orange')
+            self.caldera.create_polygon(
+                100, 140, 70, 185, 130, 185, fill='yellow')
+            self.caldera.create_polygon(100, 30, 70, 75, 130, 75, fill='white')
+            self.grados = 75
+
+        elif self.grados < 50:
+            self.caldera.create_polygon(
+                100, 140, 70, 185, 130, 185, fill='yellow')
+            self.caldera.create_polygon(
+                100, 85, 70, 130, 130, 130, fill='white')
+            self.caldera.create_polygon(100, 30, 70, 75, 130, 75, fill='white')
+            self.grados = 50
+        print(self.grados)
+
+    def bajar_temperatura(self):
+        if self.grados >= 50 and self.grados < 75:
+            self.caldera.create_polygon(
+                100, 140, 70, 185, 130, 185, fill='white')
+            self.caldera.create_polygon(
+                100, 85, 70, 130, 130, 130, fill='white')
+            self.caldera.create_polygon(100, 30, 70, 75, 130, 75, fill='white')
+            self.grados = 25
+
+        elif self.grados >= 75 and self.grados < 100:
+            self.caldera.create_polygon(
+                100, 85, 70, 130, 130, 130, fill='white')
+            self.caldera.create_polygon(
+                100, 140, 70, 185, 130, 185, fill='yellow')
+            self.caldera.create_polygon(100, 30, 70, 75, 130, 75, fill='white')
+            self.grados = 50
+
+        elif self.grados >= 100:
+            self.caldera.create_polygon(100, 30, 70, 75, 130, 75, fill='white')
+            self.caldera.create_polygon(
+                100, 140, 70, 185, 130, 185, fill='yellow')
+            self.caldera.create_polygon(
+                100, 85, 70, 130, 130, 130, fill='orange')
+            self.grados = 75
+        print(self.grados)
 
     def crear(self):
         self.caldera.pack()
 
-        boton = Button(raiz, bg="magenta", text="LLENAR1",
+        boton = Button(self.frame, bg="magenta", text="LLENAR1",
                        command=self.llenado1, width=15)
         boton.pack()
         boton. place(x=380, y=120)
 
-        boton3 = Button(raiz, bg="magenta", text="LLENAR2",
+        boton3 = Button(self.frame, bg="magenta", text="LLENAR2",
                         command=self.llenado2, width=15)
         boton3.pack()
         boton3. place(x=380, y=80)
 
-        boton2 = Button(raiz, bg="aqua", text="VACIAR",
-                        command=self.vaciado, width=15)
+        boton2 = Button(self.frame, bg="aqua", text="VACIAR TODO",
+                        command=self.vaciado2)
         boton2.pack()
-        boton2. place(x=380, y=170)
+        boton2. place(x=380, y=10)
 
-        """temp = uniform(0, 100)
-        llenado1()
-        time.sleep(0.5)
-        if temp > 80:
-            vaciado()
-        else:
-            llenado2()
-            time.sleep(0.5)
-            vaciado()"""
+        boton1 = Button(self.frame, bg="aqua", text="VACIAR 1",
+                        command=self.vaciado1)
+        boton1.pack()
+        boton1. place(x=380, y=40)
+
+        boton4 = Button(self.frame, bg="aqua", text="Subir temp",
+                        command=self.subir_temperatura)
+        boton4.pack()
+        boton4. place(x=20, y=120)
+
+        boton5 = Button(self.frame, bg="aqua", text="Bajar temp",
+                        command=self.bajar_temperatura)
+        boton5.pack()
+        boton5. place(x=20, y=170)
 
 
 raiz = Tk()
